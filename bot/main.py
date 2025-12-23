7dd60c36b991e239fc5863fb7f5bc1e070e7d42a
salsadudemon <antony@garrisi.eu>
2025-12-23 14:23:33 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,16 @@
 from reader import load_prospects
 from logic import split_by_state
 from writer import print_summary, write_synthesis
+import os
 
-FILEPATH = "data/prospects.xlsx"
+FILEPATH = os.path.join("data", "prospects.xlsx")
 
 def main():
     df = load_prospects(FILEPATH)
     groups = split_by_state(df)
     print_summary(groups)
     write_synthesis(groups)
+    input("\nAppuyez sur Entrée pour fermer...")
 
 if __name__ == "__main__":
     main()
