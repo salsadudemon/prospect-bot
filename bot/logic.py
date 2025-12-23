de939770c21ab25af6f65f5ab751deae60a7aa9a
salsadudemon <antony@garrisi.eu>
2025-12-23 14:11:02 +0100

    Add logic to split prospects by state

--- bot/logic.py
+++ bot/logic.py
@@ -1,1 +1,8 @@
+def split_by_state(df):
+    result = {
+        "A_CONTACTER": df[df["etat"] == "A_CONTACTER"],
+        "RELANCE": df[df["etat"] == "RELANCE"],
+        "INACTIF": df[df["etat"] == "INACTIF"],
+    }
+    return result
 
